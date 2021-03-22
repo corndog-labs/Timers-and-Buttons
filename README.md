01a5b9f62cfc200e066ca97eadfc39607d133613
corndog-labs <56854559+corndog-labs@users.noreply.github.com>
2021-03-21 21:48:27 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Buttons and Timers 
+#### by Courtney Crother 
+
+
+
+### Overview
+This is an assignment for Interaction Design
+It shows 
+
+### Technical Details
+
+Modify the sketch.js setup() and draw() functions and begin adding functions!
+
+##Functions 
---
+++ sketch.js
@@ -0,0 +1,22 @@
+/***********************************************************************************
+  BUTTONS AND TIMERS
+  by COURTNEY CROTHER 
+  
+  DESCRIPTION:
+
+  Very simple example of buttons and timers
+  I used some of my collage work to create some movement 
+
+***********************************************************************************/
+
+// Setup code goes here
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+ }
+
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+}

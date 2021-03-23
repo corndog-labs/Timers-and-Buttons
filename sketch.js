c3672b9a4c6c12367a8301e91d88e5ffe8119ad0
corndog-labs <56854559+corndog-labs@users.noreply.github.com>
2021-03-22 22:55:40 -0700

    comitt
    looks okay, button still weird

--- sketch.js
+++ sketch.js
@@ -4,55 +4,54 @@
   Timer by Scott Kidall 
   
   DESCRIPTION:
-
   Very simple example of buttons and timers
   this makes you pick between two animal crossing charaters 
 
+need help with making the buttons clickable - maybe I am doing the functions wrong  
 ***********************************************************************************/
-// Global Variables
 
+// Global Variables
 var tiaButton;
 var pietroButton;
-var wrongButton
 var simpleTimer;
-var correctButton;
 var yTextPos = 60;
+let bg;
 
 //images 
 imgAnimal = [];
 
-
-//load photos 
 function preload() {
 	//load images
 	imgAnimal[0] = loadImage('assets/Tia_NH.png');
 	imgAnimal[1] = loadImage('assets/Pietro_NH.png');
-	imgAnimal[2] = loadImage('assets/acnh_back.jpg');
+
 }
 
 //center things, call timers and buttons
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-	imageMode(CENTER);
+	bg = loadImage('assets/acnh_back.jpg');
+  	createCanvas(1990, 1080);
+	// imageMode(CENTER);
 	textAlign(CENTER);
 	textSize(25);
  
- //make simple timer 10 seconds 
-  simpleTimer = new Timer(10000);
-  simpleTimer.start();
+ 	//make simple timer 10 seconds 
+  	simpleTimer = new Timer(10000);
+  	simpleTimer.start();
 
  	// make buttons
 	makeTiaButton();
 	makePietroButton();
-
+	//make functions 
+	makeDrawCorrect();
+	makeDrawWrong();
  }
 
 
  function draw() {
-  background(255, 255,255);
+  background(bg);
   frameRate(60);
- 
   textSize(45);
   fill(200, 30, 90);
   text("Quickly! Which Villager is cuter?", width/2, height/8);
@@ -63,13 +62,12 @@ function setup() {
  //draw buttons
  tiaButton.draw();
  pietroButton.draw();
-
 }
 
 function makeTiaButton(){
-
 	//make image a button
 	tiaButton = new Clickable();
+
 	// set image
 	tiaButton.image = imgAnimal[0]; 
 	tiaButton.text = "";
@@ -83,7 +81,9 @@ function makeTiaButton(){
   	tiaButton.height = imgAnimal[0].height;
 
 	//location of button 
-	tiaButton.locate(2*(width/5) - tiaButton.width/2, height/1.5 - tiaButton.height/2);
+	tiaButton.locate(2*(width/7) - tiaButton.width/2, height/1.5 - tiaButton.height/2);
+
+	tiaButton.onPress = tiaButtonPressed;
 }
 
 
@@ -105,40 +105,40 @@ function makePietroButton () {
   	pietroButton.height = imgAnimal[1].height;
 
 	//location 
-	pietroButton.locate(2*(width/2.5) - pietroButton.width/2, height/1.5 - pietroButton.height/2);
+	pietroButton.locate(2*(width/3) - pietroButton.width/2, height/1.5 - pietroButton.height/2);
+
+	pietroButton.onPress = pietroButtonPressed;
+}
+
+ function makeDrawWrong () {
+	textSize(40);
+	fill(0, 0, 255 );
+	text('Wrong answer', 1000 , 500);
 
 }
 
+ function makeDrawCorrect () {
+	textSize(40);
+	fill(0, 225, 0 );
+	text('Correct', 1000, 500);
+}
 
 tiaButtonPressed = function() {
-	drawFunction = drawWrong;
-	text('wrong', 400, 400);
+	drawFunction = makeDrawWrong;
 
 }
 
 pietroButtonPressed = function() {
-	drawFunction = drawCorrect;
+	drawFunction = makeDrawCorrect;
 	
 }
 
-drawWrong = function() {
-	textSize(20);
-	fill(255, 0, 0 );
-	text('Wrong', 1000 , 1000);
-
-}
-
-drawCorrect = function() {
-	textSize(20);
-	fill(0, 225, 0 );
-	text('Correct', 1000, 1000);
-}
 
 function updateTimer() {
   if( simpleTimer.expired() ) {
   	fill(255,0,0);
 	textSize(100);
-	text( "Out of Time!", 800, 1000);
+	text( "Out of Time!", 1000, 500);
 	frameRate(2);
   }
   else {
